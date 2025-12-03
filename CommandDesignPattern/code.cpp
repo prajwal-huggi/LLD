43ee6220249e043a9c7823c5d5295536794ba307
prajwal-consultadd <prajwal.h@consultadd.com>
2025-12-03 21:21:59 +0530

    comamnd design pattern 2.0

--- CommandDesignPattern/code.cpp
+++ CommandDesignPattern/code.cpp
@@ -1,106 +1,96 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <memory>
 using namespace std;
 
-class ICommand{
-    public:
-    virtual void execute()= 0;
-    virtual void undo()= 0;
-    virtual ~ICommand(){}
+class ICommand {
+public:
+    virtual void execute() = 0;
+    virtual void undo() = 0;
+    virtual ~ICommand() = default;
 };
 
-class RemoteControl{
-    vector<ICommand*> commands;
+class NoCommand : public ICommand {
+public:
+    void execute() override {}
+    void undo() override {}
+};
+
+class RemoteControl {
+    vector<unique_ptr<ICommand>> commands;
     vector<bool> isPressed;
 
-    public:
-    RemoteControl(int buttons){
-        commands.resize(buttons, nullptr);
+public:
+    RemoteControl(int buttons) {
+        commands.resize(buttons);
         isPressed.resize(buttons, false);
-    }
-    void setCommand(ICommand* command, int idx){
-        commands[idx]= command;
+
+        for (auto &cmd : commands) {
+            cmd = make_unique<NoCommand>();
+        }
     }
 
-    void buttonPressed(int idx){
-        if(!isPressed[idx]){
-            isPressed[idx]= !isPressed[idx];
-            commands[idx]-> execute();
-        }
-        else {
-            isPressed[idx]= !isPressed[idx];
-            commands[idx]-> undo();
+    void setCommand(unique_ptr<ICommand> command, int idx) {
+        commands[idx] = std::move(command);
+    }
+
+    void buttonPressed(int idx) {
+        if (!commands[idx]) return;
+
+        if (!isPressed[idx]) {
+            commands[idx]->execute();
+            isPressed[idx] = true;
+        } else {
+            commands[idx]->undo();
+            isPressed[idx] = false;
         }
     }
 };
 
-class Light{
-    public:
-    void on(){
-        cout<<"The light is switched on"<<endl;
-    }
-    void off(){
-        cout<<"The light is switched off"<<endl;
-    }
+class Light {
+public:
+    void on()  { cout << "The light is switched on" << endl; }
+    void off() { cout << "The light is switched off" << endl; }
 };
 
-class LightCommand: public ICommand{
+class LightCommand : public ICommand {
     Light* light;
-    public:
-    LightCommand(Light* light){
-        this-> light= light;
-    }
-    void execute(){
-        light-> on();
-    }
-    void undo(){
-        light-> off();
-    }
+public:
+    LightCommand(Light* light) : light(light) {}
+    void execute() override { light->on(); }
+    void undo() override { light->off(); }
 };
 
-class Fan{
-    public:
-    void on(){
-
-        cout<<"The fan is on"<<endl;
-    }
-    void off(){
-        cout<<"The fan is off"<<endl;
-    }
+class Fan {
+public:
+    void on()  { cout << "The fan is on" << endl; }
+    void off() { cout << "The fan is off" << endl; }
 };
 
-class FanCommand: public ICommand{
+class FanCommand : public ICommand {
     Fan* fan;
-
-    public:
-    FanCommand(Fan* fan){
-        this-> fan= fan;
-    }
-    void execute(){
-        fan-> on();
-    }
-    void undo(){
-        fan-> off();
-    }
+public:
+    FanCommand(Fan* fan) : fan(fan) {}
+    void execute() override { fan->on(); }
+    void undo() override { fan->off(); }
 };
 
-int main(){
+int main() {
+    RemoteControl remote(5);
 
-    RemoteControl* remote= new RemoteControl(5);
-    
-    Light* bedroomLight= new Light();
-    Fan* bedroomFan= new Fan();
+    Light bedroomLight;
+    Fan bedroomFan;
 
-    remote->setCommand(new FanCommand(bedroomFan), 1); //fan
-    remote->setCommand(new LightCommand(bedroomLight), 2); //light
+    remote.setCommand(make_unique<FanCommand>(&bedroomFan), 1);
+    remote.setCommand(make_unique<LightCommand>(&bedroomLight), 2);
 
-    cout<<"Testing light"<<endl;
-    remote->buttonPressed(2);
-    remote->buttonPressed(2);
+    cout << "Testing light" << endl;
+    remote.buttonPressed(2);
+    remote.buttonPressed(2);
 
-    cout<<"Testing fan"<<endl;
-    remote->buttonPressed(1);
-    remote->buttonPressed(1);
+    cout << "Testing fan" << endl;
+    remote.buttonPressed(1);
+    remote.buttonPressed(1);
 
     return 0;
 }
